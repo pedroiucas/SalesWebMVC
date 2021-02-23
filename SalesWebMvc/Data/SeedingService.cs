0581ae3272336c23c97a379d8103d0ee3f0b9254
pedro.constantino <pedro.constantino@enerwatt.com.br>
2021-02-23 17:36:45 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMvcContext _context;
+
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+        }
+    }
+}
